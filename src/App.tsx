7fc8cc13b1255b7e5763f7465b7c1875a5789766
telonasport99 <telonasport99@gmail.com>
2024-01-03 21:10:17 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
       <Button as={'a'} href={'/link'} variant={'primary'}>
         Hello
       </Button>
+        <Button as={'a'} href={'/link'} variant={'primary'}>
+        Buy
+      </Button>
       <Button variant={'primary'}>Hello</Button>
     </div>
   )
