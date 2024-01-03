1dbefc560f8759e922068f3e358a03ad4d534f3b
Vlad <Sp0ngbik@gmail.com>
2024-01-03 18:47:28 +0300

    add styles

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from '@storybook/react'
+import '../src/styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import App from '@/App'
 import { createRoot } from 'react-dom/client'
 
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
