8bcceb468c546af63c91f14989939ec0d530026d
Vlad <Sp0ngbik@gmail.com>
2024-01-03 15:52:57 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: '@it-incubator/eslint-config',
+  overrides: [
+    {
+      files: ['**/*.stories.tsx'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'off',
+        'no-console': 'off',
+      },
+    },
+  ],
+}
