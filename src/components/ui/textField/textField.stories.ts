c452db05ec3152ffdeb488a9538b40eb8f1b3171
Vlad <Sp0ngbik@gmail.com>
2024-01-07 13:55:41 +0300

    button with icon

--- src/components/ui/textField/textField.stories.ts
+++ src/components/ui/textField/textField.stories.ts
@@ -27,52 +27,61 @@ type Story = StoryObj<typeof meta>
 
 export const PrimaryInput: Story = {
   args: {
+    placeholder: 'Primary input',
     variant: 'text',
   },
 }
 export const PrimaryError: Story = {
   args: {
     error: true,
+    placeholder: 'Primary Error ',
     variant: 'text',
   },
 }
 export const PrimaryDisabled: Story = {
   args: {
     disabled: true,
+    placeholder: 'Primary Disabled',
     variant: 'text',
   },
 }
 export const PasswordInput: Story = {
   args: {
+    placeholder: 'Password Input',
     variant: 'password',
   },
 }
 export const PasswordError: Story = {
   args: {
     error: true,
+    placeholder: 'Password Error',
     variant: 'password',
   },
 }
 export const PasswordDisabled: Story = {
   args: {
     disabled: true,
+    placeholder: 'Password Disabled',
     variant: 'text',
   },
 }
 export const SearchInput: Story = {
   args: {
+    placeholder: 'Search Input',
     variant: 'search',
   },
 }
 export const SearchError: Story = {
   args: {
     error: true,
+    placeholder: 'Search Error',
     variant: 'search',
   },
 }
 export const SearchDisabled: Story = {
   args: {
     disabled: true,
+    placeholder: 'Search Disabled',
     variant: 'search',
   },
 }
