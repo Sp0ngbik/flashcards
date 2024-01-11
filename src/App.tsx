75108db56a5fae03b2377c88d67732c7ca1a12cd
Vlad <Sp0ngbik@gmail.com>
2024-01-11 19:52:56 +0300

    before pagination logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Button } from '@/components/ui/button/button'
 import { Header } from '@/components/ui/header'
 import { RadioGroup } from '@/components/ui/radio-group'
 
+import { Pagination } from './components/ui/pagination'
+
 function App() {
   return (
     <div>
       <Header isLoggedIn />
       <Button variant={'primary'}>Hello</Button>
       <RadioGroup options={['sad', 'dsa', 'ss', 'dd1']} />
+      <Pagination />
     </div>
   )
 }
