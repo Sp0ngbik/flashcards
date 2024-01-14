55b432ea8cf8298f8cea4756714922c0cd4a504c
Vlad <Sp0ngbik@gmail.com>
2024-01-14 14:55:01 +0300

    refactor radio group

--- src/components/ui/radio-group/radioGroup.tsx
+++ src/components/ui/radio-group/radioGroup.tsx
@@ -1,34 +1,21 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
-import { Typography } from '@/components/ui/typography'
 import * as RadioGroupRadix from '@radix-ui/react-radio-group'
 
 import s from './radioGroup.module.scss'
 
 type Props = {
   className?: string
-  defaultChecked?: boolean
-  disabled?: boolean
-  options: string[]
 } & ComponentPropsWithoutRef<typeof RadioGroupRadix.Root>
 
 export const RadioGroup = forwardRef<ElementRef<typeof RadioGroupRadix.Root>, Props>(
   (props, ref) => {
-    const { options } = props
-
     return (
-      <form>
-        <RadioGroupRadix.Root className={s.radioGroupRoot} {...props} defaultValue={options[0]}>
-          {props.options.map((el, index) => (
-            <div className={s.radioButtonBlock} key={index} ref={ref}>
-              <RadioGroupRadix.Item className={s.radioGroupItem} id={'r' + index} value={el}>
-                <RadioGroupRadix.Indicator className={s.radioGroupIndicator} />
-              </RadioGroupRadix.Item>
-              <Typography as={'label'} className={s.label} htmlFor={el} variant={'body2'}>
-                {el}
-              </Typography>
-            </div>
-          ))}
+      <form className={s.formGroup}>
+        <RadioGroupRadix.Root className={s.radioGroupRoot} {...props}>
+          <div className={s.radioButtonBlock} ref={ref}>
+            {props.children}
+          </div>
         </RadioGroupRadix.Root>
       </form>
     )
