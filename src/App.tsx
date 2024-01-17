b97d38ff18589599b056fc90d7ba6cb6486e6fce
Vlad <Sp0ngbik@gmail.com>
2024-01-17 14:10:49 +0300

    opt app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import { SignUp } from '@/components/auth/signUp'
-import { DoubleSlider } from '@/components/ui/slider/slider'
-
-import { Pagination } from './components/ui/pagination'
 
 function App() {
   return (
     <div>
-      <Pagination totalCount={250} />
       <SignUp />
-      <DoubleSlider defaultValue={[10, 25]} max={25} min={10} />
     </div>
   )
 }
