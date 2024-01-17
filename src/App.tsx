160c01397efe1ce01eff3e811913ae28499ce2d8
Vlad <Sp0ngbik@gmail.com>
2024-01-17 14:13:18 +0300

    opt app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { SignUp } from '@/components/auth/signUp'
+import { DoubleSlider } from '@/components/ui/slider'
+
+import { SignIn } from './components/auth/signIn'
 
 function App() {
   return (
     <div>
+      <DoubleSlider defaultValue={[10, 15]} max={15} min={10} />
       <SignUp />
+      <SignIn />
     </div>
   )
 }
