12e2df8034910b21a344da342853c03e2a6b0f79
telonasport99 <telonasport99@gmail.com>
2024-01-19 14:50:16 +0300

    Automatic Password

--- src/components/auth/createNewPassword/createNewPassword.stories.tsx
+++ src/components/auth/createNewPassword/createNewPassword.stories.tsx
@@ -1,5 +1,6 @@
 import CreateNewPassword from '@/components/auth/createNewPassword/createNewPassword'
 import { Meta, StoryObj } from '@storybook/react'
+import { userEvent, within } from '@storybook/test'
 
 const meta = {
   component: CreateNewPassword,
@@ -12,4 +13,16 @@ type Story = StoryObj<typeof meta>
 
 export const CreateNewPasswordAuth: Story = {
   args: {},
+}
+export const DelayedStory: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const exampleElement = canvas.getByPlaceholderText('Password')
+
+    // The delay option sets the amount of milliseconds between characters being typed
+    await userEvent.type(exampleElement, 'random string', {
+      delay: 100,
+    })
+  },
 }
