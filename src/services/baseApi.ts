1c44418561abf344708d22bc2ded0b445060a1f2
Vlad <Sp0ngbik@gmail.com>
2024-01-28 22:35:19 +0300

    JWT

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -1,10 +1,8 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { baseQueryWithReauth } from '@/services/base-api-with-reauth'
+import { createApi } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.flashcards.andrii.es',
-    credentials: 'include',
-  }),
+  baseQuery: baseQueryWithReauth,
   endpoints() {
     return {}
   },
