3a42a50ea9df02aa084fbc95488be2d14f6e57b0
Vlad <Sp0ngbik@gmail.com>
2024-02-02 12:11:46 +0300

    fix cards

--- src/pages/auth/signUp/signUp.tsx
+++ src/pages/auth/signUp/signUp.tsx
@@ -50,47 +50,49 @@ export const SignUp = () => {
   }
 
   return (
-    <Card>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Typography className={s.signUpLabel} variant={'large'}>
-          Sign Up
+    <div className={s.signUpWrapper}>
+      <Card>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Typography className={s.signUpLabel} variant={'large'}>
+            Sign Up
+          </Typography>
+          <TextFieldControlled
+            control={control}
+            errorMessage={errors.email?.message}
+            id={'email'}
+            label={'Email'}
+            name={'email'}
+            placeholder={'example@gmail.com'}
+          />
+          <TextFieldControlled
+            className={s.passwordField}
+            control={control}
+            errorMessage={errors.password?.message}
+            id={'password'}
+            label={'Password'}
+            name={'password'}
+            placeholder={'Your password'}
+            variant={'password'}
+          />
+          <TextFieldControlled
+            className={s.confirmPassword}
+            control={control}
+            errorMessage={errors.confirmPassword?.message}
+            id={'confirmPassword'}
+            label={'Confirm Password'}
+            name={'confirmPassword'}
+            placeholder={'Confirm your password'}
+            variant={'password'}
+          />
+          <Button fullWidth>Sign Up</Button>
+        </form>
+        <Typography className={s.formQuestion} variant={'body2'}>
+          Already have an account?
         </Typography>
-        <TextFieldControlled
-          control={control}
-          errorMessage={errors.email?.message}
-          id={'email'}
-          label={'Email'}
-          name={'email'}
-          placeholder={'example@gmail.com'}
-        />
-        <TextFieldControlled
-          className={s.passwordField}
-          control={control}
-          errorMessage={errors.password?.message}
-          id={'password'}
-          label={'Password'}
-          name={'password'}
-          placeholder={'Your password'}
-          variant={'password'}
-        />
-        <TextFieldControlled
-          className={s.confirmPassword}
-          control={control}
-          errorMessage={errors.confirmPassword?.message}
-          id={'confirmPassword'}
-          label={'Confirm Password'}
-          name={'confirmPassword'}
-          placeholder={'Confirm your password'}
-          variant={'password'}
-        />
-        <Button fullWidth>Sign Up</Button>
-      </form>
-      <Typography className={s.formQuestion} variant={'body2'}>
-        Already have an account?
-      </Typography>
-      <Button className={s.submitButton} onClick={handleSignInClick} variant={'link'}>
-        Sign In
-      </Button>
-    </Card>
+        <Button className={s.submitButton} onClick={handleSignInClick} variant={'link'}>
+          Sign In
+        </Button>
+      </Card>
+    </div>
   )
 }
