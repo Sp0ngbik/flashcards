3a545ec580a37ae2817ccac66fca188bfe068a18
Vlad <Sp0ngbik@gmail.com>
2024-02-02 15:41:35 +0300

    refactor decks and cards

---
+++ src/pages/decs/ui/deckRow/deckRow.tsx
@@ -0,0 +1,68 @@
+import { Delete, Edit, Play } from '@/assets'
+import { Button } from '@/common/ui/button'
+import { TableDataCell, TableRow } from '@/common/ui/table/tableConstuctor'
+import { EditDeckType } from '@/features/deck/deckForm/DeckForm'
+import { Deck } from '@/services/decks/decks.types'
+import { clsx } from 'clsx'
+
+import s from './deckRow.module.scss'
+
+type DeckRowProps = {
+  deck: Deck
+  deleteDeck: (id: string) => void
+  isDeleted: boolean
+  isOwner: boolean
+  openDeck: (id: string) => void
+  openEditMode: (deck: EditDeckType) => void
+}
+
+const DeckRow = ({
+  deck,
+  deleteDeck,
+  isDeleted,
+  isOwner,
+  openDeck,
+  openEditMode,
+}: DeckRowProps) => {
+  const classNames = {
+    icon: clsx(s.icon, isDeleted && s.disableIcon),
+  }
+
+  const openDeckHandler = () => {
+    openDeck(deck.id)
+  }
+  const deleteDeckHandler = () => {
+    deleteDeck(deck.id)
+  }
+
+  const openEditModeHandler = () => {
+    openEditMode(deck)
+  }
+
+  return (
+    <TableRow key={deck.id}>
+      <TableDataCell>
+        <Button className={s.tableDataContent} onClick={openDeckHandler} variant={'link'}>
+          {deck.cover && <img alt={'image'} className={s.tableImage} src={deck.cover} />}
+          {deck.name}
+        </Button>
+      </TableDataCell>
+      <TableDataCell>{deck.cardsCount}</TableDataCell>
+      <TableDataCell>{new Date(deck.updated).toLocaleDateString('ru-RU')}</TableDataCell>
+      <TableDataCell>{deck.author.name}</TableDataCell>
+      <TableDataCell>
+        {isOwner ? (
+          <>
+            <Edit className={s.icon} onClick={openEditModeHandler} />
+            <Play className={s.icon} />
+            <Delete className={classNames.icon} onClick={deleteDeckHandler} />
+          </>
+        ) : (
+          <Play className={s.icon} />
+        )}
+      </TableDataCell>
+    </TableRow>
+  )
+}
+
+export default DeckRow
