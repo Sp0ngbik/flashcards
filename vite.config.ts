cce7aede351660bb23e609ded92df331bbacc382
Vlad <Sp0ngbik@gmail.com>
2024-02-04 15:40:58 +0300

    check safe

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
