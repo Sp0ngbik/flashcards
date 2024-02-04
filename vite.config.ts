e716ffb989c503e25afafaf2dcfd9c7bdab8b32e
Vlad <Sp0ngbik@gmail.com>
2024-02-04 15:31:40 +0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
