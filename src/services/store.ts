2194e2523196b5ba16046831b4b1e8310638082a
Vlad <Sp0ngbik@gmail.com>
2024-02-09 13:17:02 +0300

    remove auth slice

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,6 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
-import { authReducer } from '@/services/auth/authSlice'
 import { baseApi } from '@/services/baseApi'
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
@@ -8,7 +7,6 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 export const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
   reducer: {
-    auth: authReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
 })
